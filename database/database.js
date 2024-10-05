f75b9c96f54b5b2a89a3210d8fc801d0d9ad8ffe
juancmdev <juancmorenoj@gmail.com>
2024-10-05 18:56:38 -0500

    updated

--- database/database.js
+++ database/database.js
@@ -14,6 +14,52 @@ export const animals = [
     back: "Elephant",
     alt: "Elephant image",
   },
+
+  {
+    img: "/imgs/animals/horse.jpg",
+    back: "Horse",
+    alt: "Horse image",
+  },
+  {
+    img: "/imgs/animals/cat.jpg",
+    back: "Cat",
+    alt: "Cat image",
+  },
+  {
+    img: "/imgs/animals/chicken.jpg",
+    back: "Chicken",
+    alt: "Chicken image",
+  },
+  {
+    img: "/imgs/animals/hippo.jpg",
+    back: "Hippo",
+    alt: "Hippo image",
+  },
+  {
+    img: "/imgs/animals/hen.jpg",
+    back: "Hen",
+    alt: "Hen image",
+  },
+  {
+    img: "/imgs/animals/worm.jpg",
+    back: "Worm",
+    alt: "Worm image",
+  },
+  {
+    img: "/imgs/animals/bear.jpg",
+    back: "Bear",
+    alt: "Bear image",
+  },
+  {
+    img: "/imgs/animals/butterfly.jpg",
+    back: "Butterfly",
+    alt: "Butterfly image",
+  },
+  {
+    img: "/imgs/animals/donkey.jpg",
+    back: "Donkey",
+    alt: "Donkey image",
+  },
 ];
 
 export const partsOfHouse = [
