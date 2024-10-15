b380bafaeb82a2e2ff48b2da14c54bbb5f4022f0
juancmdev <juancmorenoj@gmail.com>
2024-10-14 19:22:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const timeout = document.querySelector(".timeout");
 const btnAlert = document.querySelector(".btn-alert");
 
 setTimeout(() => {
+  displayNone();
   timeout.classList.add("display");
 }, 3000);
 
 btnAlert.addEventListener("click", () => {
   timeout.classList.add("display-none");
 });
+
+function displayNone() {
+  console.log("funciona");
+
+  //   setTimeout(() => {
+  //     timeout.classList.remove("display");
+  //   }, 3000);
+}
