b1558706de94eff64a2eca528b645ad0013f873a
juancmdev <juancmorenoj@gmail.com>
2025-03-09 21:32:08 -0500

    updated

---
+++ database/personal_pronouns_db.js
@@ -0,0 +1,30 @@
+export const personal_pronouns = [
+  {
+    front: "Yo",
+    back: "I",
+  },
+  {
+    front: "Tú, ustedes",
+    back: "You",
+  },
+  {
+    front: "Él (Persona)",
+    back: "He",
+  },
+  {
+    front: "Ella (Persona)",
+    back: "She",
+  },
+  {
+    front: "Él, eso (Animal, cosa)",
+    back: "It",
+  },
+  {
+    front: "Nosotros",
+    back: "We",
+  },
+  {
+    front: "Ellos",
+    back: "They",
+  },
+];
