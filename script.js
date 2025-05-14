0f210507cf5fdb669126501e7d5e4be0235de7dd
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-05-14 14:46:39 -0500

    updated

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const timeout = document.querySelector(".timeout");
 const btnAlert = document.querySelector(".btn-alert");
 
+const btnCategories = document.querySelector(".btn-categories");
+
+btnCategories.addEventListener("mouseenter", () => {
+  console.log('Funciona');
+  
+})
+
 setTimeout(() => {
   timeout.classList.add("display");
 }, 3000);
