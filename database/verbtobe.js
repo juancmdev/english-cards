9c4db9abc565f88eae9337394849b24cfe3646d5
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-05-15 09:00:20 -0500

    updated

---
+++ database/verbtobe.js
@@ -0,0 +1,34 @@
+export const verbtobe = [
+  {
+    front: "I am",
+    back: "Yo soy o Yo estoy",
+  },
+  {
+    front: "You are",
+    back: "Tú eres o Tú estás",
+  },
+  {
+    front: "He is",
+    back: "Él es o Él está",
+  },
+  {
+    front: "She is",
+    back: "Ella es o Ella está",
+  },
+  {
+    front: "It is",
+    back: "Eso es o Eso está",
+  },
+  {
+    front: "We are",
+    back: "Nosotros somos o Nosotros estamos",
+  },
+  {
+    front: "You are",
+    back: "Nosotros somos o Nosotros estamos",
+  },
+  {
+    front: "They are",
+    back: "Ellos son o Ellos están",
+  },
+];
